dcfbe31f312b142dd652e979bfa9a5d13f17e823
Ronaldo <ronaldofilho2@hotmail.com>
2020-10-08 18:03:18 -0300

    Testes e Implementação da API

---
+++ src/test/java/com/vuepetapi/vuepetapi/controller/DogControllerTest.java
@@ -0,0 +1,32 @@
+package com.vuepetapi.vuepetapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogControllerTest {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void addVet() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAllByNome() {
+    }
+}
---
+++ src/test/java/com/vuepetapi/vuepetapi/controller/VetControllerTest.java
@@ -0,0 +1,28 @@
+package com.vuepetapi.vuepetapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetControllerTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAllDogs() {
+    }
+
+    @Test
+    void findAllByNome() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
